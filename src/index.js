b21deb19350e8a44da4b41eb0348d7065a021071
servet-isikli <servetisikli@yahoo.com>
2023-06-05 23:33:48 +0200

    formated

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-
 import App from './App';
-
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
